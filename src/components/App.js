6f1f59315e8783d7b1c2fc054002033fa0e88279
Mayuri Rathod <mayuri.gr@gmail.com>
2019-03-09 15:09:50 +0800

    Added VideoDetail component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "./apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 export default class App extends React.Component {
   state = { videos: [], selectedVideo: null };
@@ -17,17 +18,28 @@ export default class App extends React.Component {
   };
 
   onVideoSelect = video => {
-    console.log("in App", video);
+    this.setState({
+      selectedVideo: video
+    });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList
-          onVideoSelect={this.onVideoSelect}
-          videos={this.state.videos}
-        />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                onVideoSelect={this.onVideoSelect}
+                videos={this.state.videos}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,7 +3,13 @@ import VideoItem from "./VideoItem";
 
 const VideoList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map(eachVideo => {
-    return <VideoItem onVideoSelect={onVideoSelect} video={eachVideo} />;
+    return (
+      <VideoItem
+        key={eachVideo.id.videoId}
+        onVideoSelect={onVideoSelect}
+        video={eachVideo}
+      />
+    );
   });
   return <div className="ui relaxed divided list">{renderedList}</div>;
 };
